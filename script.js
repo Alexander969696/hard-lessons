dbb2703203535986f1550ea4f710968069164a9b
Aleksander Afonin <alexanderafonin96@gmail.com>
2022-03-30 03:24:33 +0600

    Выполнил задания к 3 уроку

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-let num = 266219;
-let str = String(num); //преобразуем к строке, чтобы был доступ к каждому символу
+'use strict'
 
-let result = 1;
+// 1 задание
 
-for (let i = 0; i < str.length; i++) {
-    result *= str[i]; //при использовании математических операторов
-                      //происходит автоматическое преобразование к числу
+let lang = prompt('Введите язык страницы (ru или en)');
+
+while (lang !== 'ru' && lang !== 'en') { 
+    lang = prompt('Введите ru или en');
 }
 
-console.log(result);
+if (lang == 'ru') {
+    console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+} else console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
 
-result = String(result ** 3);    // возводим в степень с помощью стандартного оператора
-console.log(result.slice(0, 2)); //и сразу преобразуем к строке, чтобы получить подстроку
+switch (lang) {
+    case 'ru':
+        console.log('Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье');
+        break;
+    case 'en':
+        console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+        break;
+}
+
+let arr = [
+    ['Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'],
+    ['Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday']
+]
+
+let i = (lang == 'ru') ? 0 : 1;
+console.log(arr[i]);
+
+// 2 задание
+
+let namePerson = prompt('Введите имя:');
+
+let status = (namePerson == 'Артем') ? 'Директор' :
+  (namePerson == 'Александр') ? 'Преподаватель' : 'Студент';
+
+console.log(status);
