cd5bbc75a6af9e1d9afa11a0e2b1741b1d9f4a0f
Aleksander Afonin <alexanderafonin96@gmail.com>
2022-03-30 23:55:31 +0600

    исправление задания к 4 уроку

--- script.js
+++ script.js
@@ -4,50 +4,18 @@ let str = '   Lorem ipsum dolor sit amet, consectetur adipiscing elit,sed do eiu
 let notStr = 88;
 
 
-const deleteStartSpace = function (str) {
-    let newStr = '';
-
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] === ' ') {
-            continue;
-         } else {
-            newStr = str.slice(i);
-            break;
-         }
-    }
-    return newStr;
-}
-
-const reverseString = function (str) {
-    let reverseStr = '';
-
-    for (let i = str.length - 1; i >= 0; i--) {
-        reverseStr += str[i];
-    }
-    return reverseStr;
-}
-
-const getSubstr = function (str) {
-    let ellipsis = '...';
-    str = str.slice(0, 30) + ellipsis;
-    return str;
-}
-
 const mainFunc = function (str) {
-    let resultStr;
-
     if (typeof(str) !== 'string') {
         return 'Вы ввели не строку';
     }
-    resultStr = deleteStartSpace(str);
-    resultStr = reverseString(resultStr);
-    resultStr = deleteStartSpace(resultStr);
-    resultStr = reverseString(resultStr);
 
-    if (resultStr.length > 30) return getSubstr(resultStr);
-
-    return resultStr;
-}
+    let text = str.trim();
+    if (text.length > 30) {
+        return text.slice(0, 30) + '...';
+    } else {
+        return text;
+    }
+};
 
 console.log(mainFunc(str));
 console.log(mainFunc(notStr));
